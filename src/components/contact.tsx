3e463db020667c8bfc8a3f5e84f0e048adac61a3
“Ahmed <ahmedmostafa4497@gmail.com>
2025-05-12 22:58:24 +0300

    projects and contact sections

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,8 +1,123 @@
+import PhoneOutlinedIcon from '@mui/icons-material/PhoneOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import ArrowUpwardOutlinedIcon from '@mui/icons-material/ArrowUpwardOutlined';
 
 function Contact() {
   return (
-    <section id="contact" className="bg-gradient-to-br from-gray-800 to-gray-900 min-h-screen flex items-center">
-      <h1 className="text-white ">Contact</h1>
+    <section id="contact" className="bg-gray-900 min-h-screen flex flex-col justify-center items-center px-18 py-12">
+       <p className="text-cyan-300 font-mono text-lg">Contact</p>
+       <h1 className="text-white text-4xl font-bold my-2">Get In Touch</h1>
+       <span className=" h-1.5  w-22 bg-cyan-600 my-4" />
+       <p className="text-gray-400 text-lg w-full lg:w-1/2 text-center font-bold">Feel free to reach out to me for any inquiries or opportunities</p>
+       <div className="my-8 flex flex-col lg:flex-row gap-4 lg:justify-center w-full">
+        <div className="w-full lg:w-1/3 flex flex-col gap-4 rounded-lg border border-gray-500 px-6 py-4">
+        <h2 className="text-white text-xl font-bold mx-4 my-2 w-fit">Contact Information</h2>
+        <div className='flex items-center gap-4'>
+            <div className='h-8 w-8 bg-cyan-800 rounded-full flex justify-center items-center'>
+            <PhoneOutlinedIcon fontSize='small' className='text-cyan-400 '/>
+            </div>
+            <span className='flex flex-col justify-center'>
+            <h2 className='text-white text-base font-bold'>Phone</h2>
+            <a href='tel:+201016718107' target="_blank" className="text-gray-400 text-sm  w-full lg:w-1/2 text-center font-bold">+201016718107</a>
+            </span>
+        </div>
+        <div className='flex items-center gap-4'>
+            <div className='h-8 w-8 bg-cyan-800 rounded-full flex justify-center items-center'>
+            <EmailOutlinedIcon fontSize='small' className='text-cyan-400 '/>
+            </div>
+            <span className='flex flex-col justify-center'>
+            <h2 className='text-white text-base font-bold'>Email</h2>
+            <a href='mailto:ahmedmostafa4497@gmail.com' target="_blank" className="text-gray-400 text-sm  w-full lg:w-1/2 text-center font-bold">ahmedmostafa4497@gmail.com</a>
+            </span>
+        </div>
+        <div className='flex items-center gap-4'>
+            <div className='h-8 w-8 bg-cyan-800 rounded-full flex justify-center items-center'>
+            <LocationOnOutlinedIcon fontSize='small' className='text-cyan-400 '/>
+            </div>
+            <span className='flex flex-col justify-center'>
+            <h2 className='text-white text-base font-bold'>Location</h2>
+            <a href='#' className="text-gray-400 text-sm whitespace-nowrap font-bold">
+                Cairo, Egypt
+            </a>
+            </span>
+        </div>
+        <div className='flex items-center gap-4'>
+            <div className='h-8 w-8 bg-cyan-800 rounded-full flex justify-center items-center'>
+            <GitHubIcon fontSize='small' className='text-cyan-400 '/>
+            </div>
+            <span className='flex flex-col justify-center'>
+            <h2 className='text-white text-base font-bold'>Github</h2>
+            <a href='https://github.com/ahmedmostafa2002' target="_blank" className="text-gray-400 text-sm whitespace-nowrap font-bold">
+            github.com/ahmedmostafa2002
+            </a>
+            </span>
+        </div>
+        <div className='flex items-center gap-4'>
+            <div className='h-8 w-8 bg-cyan-800 rounded-full flex justify-center items-center'>
+            <LinkedInIcon fontSize='small' className='text-cyan-400 '/>
+            </div>
+            <span className='flex flex-col justify-center'>
+            <h2 className='text-white text-base font-bold'>LinkedIn</h2>
+            <a href='https://www.linkedin.com/in/ahmed-mostafa-50364b280' target="_blank" className="text-gray-400 text-sm whitespace-nowrap font-bold">
+            linkedin.com/in/ahmed-mostafa
+            </a>
+            </span>
+        </div>
+
+
+        </div>
+        <div className="w-full lg:w-1/3 flex flex-col gap-4 rounded-lg border border-gray-500  px-6 py-4">
+        <h2 className="text-white text-xl font-bold my-2 w-fit">Send Me a Message</h2>
+        <div className='flex flex-col gap-3'>
+        <label htmlFor="name" className='text-gray-400 text-sm font-semibold'>Name</label>
+        <input type="text" placeholder="Your name" className='text-gray-400 border border-gray-500 py-1 px-3 rounded-md'/>
+        </div>
+        <div className='flex flex-col gap-3'>
+        <label htmlFor="name" className='text-gray-400 text-sm font-semibold'>Email</label>
+        <input type="text" placeholder="Your email" className='text-gray-400 border border-gray-500 py-1 px-3 rounded-md'/>
+        </div>
+        <div className='flex flex-col gap-3'>
+        <label htmlFor="name" className='text-gray-400 text-sm font-semibold'>Subject</label>
+        <input type="text" placeholder="subject" className='text-gray-400 border border-gray-500 py-1 px-3 rounded-md'/>
+        </div>
+        <div className='flex flex-col gap-3'>
+        <label htmlFor="name" className='text-gray-400 text-sm font-semibold'>Message</label>
+        <textarea 
+            placeholder="Your message" 
+            className='text-gray-400 border border-gray-500 py-1 px-3 rounded-md h-32'
+            rows={4}
+        ></textarea>
+        </div>
+        <input type="button" value="Send Message" className='bg-cyan-400 text-white text-sm py-2 rounded-md cursor-pointer font-semibold'/>
+        </div>
+       </div>
+       <div className='flex w-full justify-between mt-8 '>
+        <div className='flex flex-col justify-center items-start'>
+        <h2 className="font-bold text-white text-xl">Ahmed<span className="text-2xl text-cyan-400">.</span></h2>
+        <p className="text-gray-400 text-sm whitespace-nowrap text-center font-bold">Full Stack Web Developer</p>
+
+        </div>
+        <div className='my-8 flex items-center gap-6'>
+               <a href="https://github.com/ahmedmostafa2002" target="_blank" rel="noopener noreferrer">
+                <GitHubIcon fontSize='medium' className="text-white hover:text-cyan-400 transition-all duration-300 ease-in-out"/>
+               </a>
+               <a href="https://www.linkedin.com/in/ahmed-mostafa-50364b280" target="_blank" rel="noopener noreferrer">
+                <LinkedInIcon fontSize='medium' className="text-white hover:text-cyan-400 transition-all duration-300 ease-in-out"/>
+               </a>
+               <a href="mailto:ahmedmostafa4497@gmail.com" target="_blank" rel="noopener noreferrer">
+                <EmailOutlinedIcon fontSize='medium' className="text-white hover:text-cyan-400 transition-all duration-300 ease-in-out"/>
+               </a>
+            </div>
+       </div>
+       <div className='flex w-full justify-between mt-8 '>
+       <p className="text-gray-400 text-xs whitespace-nowrap text-center font-bold">© 2025 Ahmed Mostafa. All rights reserved.</p>
+       <a href="#home" target="_self" className='animate-bounce'>
+        <ArrowUpwardOutlinedIcon fontSize='medium' className="text-white hover:text-cyan-400 transition-all duration-300 ease-in-out"/>
+       </a>
+        </div>
     </section>
   );
 }
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,7 +1,59 @@
+import GitHubIcon from '@mui/icons-material/GitHub';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+
 function Projects() {
   return (
-    <section id="projects" className="bg-gradient-to-br from-gray-800 to-gray-900 min-h-screen flex items-center">
-      <h1 className="text-white">Projects</h1>
+    <section id="projects" className="relative bg-gradient-to-br from-gray-800 to-gray-900 min-h-screen flex flex-col justify-center items-center px-18 py-12">
+       <p className="text-cyan-300 font-mono text-lg">Projects</p>
+       <h1 className="text-white text-4xl font-bold my-2">My Work</h1>
+       <span className=" h-1.5  w-22 bg-cyan-600 my-4" />
+       <p className="text-gray-400 text-lg w-full lg:w-1/2 text-center font-bold">A collection of my work that showcases my skills and experience</p>
+       <div className="flex flex-col lg:flex-row flex-wrap lg:justify-center my-8 gap-4">
+        <span className="flex flex-col w-full lg:w-5/18 bg-gray-900 rounded-lg border border-gray-500">
+        <img src="projects/e-commerce.png" alt="portofolio project" className="w-full h-50 "/>
+        <h2 className="text-white text-2xl font-bold mx-4 my-2 w-fit">E-commerce Website</h2>
+        <p className="text-gray-400 text-base w-9/10 text-start font-semibold mx-4 break-words">A team-based e-commerce platform built with Angular, HTML, CSS, and Bootstrap at ITI.</p>
+        <div className='flex flex-wrap gap-3 mx-4 my-5 w-full'>
+          <span className='text-gray-300 hover:bg-cyan-900 rounded-full h-6 flex justify-center items-center text-xs px-3 bg-gray-800'>Angular</span>
+          <span className='text-gray-300 hover:bg-cyan-900 rounded-full h-6 flex justify-center items-center text-xs px-3 bg-gray-800'>Html</span>
+          <span className='text-gray-300 hover:bg-cyan-900 rounded-full h-6 flex justify-center items-center text-xs px-3 bg-gray-800'>Css</span>
+          <span className='text-gray-300 hover:bg-cyan-900 rounded-full h-6 flex justify-center items-center text-xs px-3 bg-gray-800'>Bootstrap</span>
+        </div>
+        <a href="https://github.com/ahmedmostafa2002/angular-project" target="_blank" rel="noopener noreferrer" className='mx-4 mb-2 grow flex items-center gap-2 text-white hover:text-cyan-400 transition-all duration-300 ease-in-out'>
+          <GitHubIcon fontSize='medium'/> Github
+        </a>
+        </span>
+        <span className="flex flex-col w-full lg:w-5/18 bg-gray-900 rounded-lg border border-gray-500">
+        <img src="projects/quran.jpg" alt="portofolio project" className="w-full h-50 "/>
+        <h2 className="text-white text-2xl font-bold mx-4 my-2 w-fit">Quran Kareem APP</h2>
+        <p className="text-gray-400 text-base w-9/10 text-start font-semibold mx-4 break-words">A cross-platform mobile application for reading the Quran with clean and readable Arabic text.</p>
+        <div className='flex flex-wrap gap-3 mx-4 my-5 w-full'>
+          <span className='text-gray-300 hover:bg-cyan-900 rounded-full h-6 flex justify-center items-center text-xs px-3 bg-gray-800'>Flutter</span>
+          <span className='text-gray-300 hover:bg-cyan-900 rounded-full h-6 flex justify-center items-center text-xs px-3 bg-gray-800'>Dart</span>
+          <span className='text-gray-300 hover:bg-cyan-900 rounded-full h-6 flex justify-center items-center text-xs px-3 bg-gray-800'>GetX</span>
+          <span className='text-gray-300 hover:bg-cyan-900 rounded-full h-6 flex justify-center items-center text-xs px-3 bg-gray-800'>Google Fonts</span>
+        </div>
+        <a href="https://github.com/ahmedmostafa2002/Quran-Kareem" target="_blank" rel="noopener noreferrer" className='mx-4 mb-2 grow flex items-center gap-2 text-white hover:text-cyan-400 transition-all duration-300 ease-in-out'>
+          <GitHubIcon fontSize='medium'/> Github
+        </a>
+        </span>
+        <span className="flex flex-col w-full lg:w-5/18 bg-gray-900 rounded-lg border border-gray-500">
+        <img src="projects/portofolio.PNG" alt="portofolio project" className="w-full h-50 "/>
+        <h2 className="text-white text-2xl font-bold mx-4 my-2 w-fit">Portfolio Website</h2>
+        <p className="text-gray-400 text-base w-9/10 text-start font-semibold mx-4 break-words">A personal portfolio website showcasing my skills and projects.</p>
+        <div className='flex flex-wrap gap-3 mx-4 my-5 w-full'>
+          <span className='text-gray-300 hover:bg-cyan-900 rounded-full h-6 flex justify-center items-center text-xs px-3 bg-gray-800'>React</span>
+          <span className='text-gray-300 hover:bg-cyan-900 rounded-full h-6 flex justify-center items-center text-xs px-3 bg-gray-800'>TailWind Css</span>
+          <span className='text-gray-300 hover:bg-cyan-900 rounded-full h-6 flex justify-center items-center text-xs px-3 bg-gray-800'>TypeScript</span>
+        </div>
+        <a href="https://github.com/ahmedmostafa2002/my_portofolio" target="_blank" rel="noopener noreferrer" className='mx-4 mb-2 grow flex items-center gap-2 text-white hover:text-cyan-400 transition-all duration-300 ease-in-out'>
+          <GitHubIcon fontSize='medium'/> Github
+        </a>
+        </span>
+       </div>
+       <a href="#contact" target="_self" className='absolute bottom-4 left-1/2 animate-bounce'>
+               <ArrowDownwardIcon fontSize='medium' className="text-white hover:text-cyan-400 transition-all duration-300 ease-in-out"/>
+       </a>
     </section>
   );
 }
